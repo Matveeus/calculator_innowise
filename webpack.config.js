b86a69cba5ab27bbf1d9136fb9368479ea78816c
Matvey Lapitchev <matvey.lapitchev@gmail.com>
2023-02-01 02:28:58 +0300

    the structure of the js files has been changed, the webpack configuration has been redone, bugs have been fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default;
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -8,6 +9,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    output: {
+                        comments: false,
+                    },
+                },
+            }),
+        ],
     },
     module: {
         rules: [
